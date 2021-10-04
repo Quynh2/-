63af3a5ea3d7921cff33993462d40ebca74da52b
Quynh2 <91867500+Quynh2@users.noreply.github.com>
2021-10-04 18:13:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,14 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Hi',
+    introDesc: `I just wanna tell you`,
+    btnIntro: '<3',
+    title: 'Im looking for my beaty 😙',
+    desc: 'and...I met you ',
+    btnYes: 'So...will you be my beauty?',
+    btnNo: 'refuse :3',
+    question:'Do you know that cute is a kind of power <3',
+    btnReply: 'Answer'
+    reply: 'Because you have a superpower <33333333',
+    messDesc: 'chị chóng khỏi ốm nhé ^.^.',
+    btnAccept:  '<3',
 }
