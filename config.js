d6201440276d4f37c1f4a09cb994bb926eda83e6
Quynh2 <91867500+Quynh2@users.noreply.github.com>
2021-10-04 18:16:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const CONFIG = {
     btnYes: 'So...will you be my beauty?',
     btnNo: 'refuse :3',
     question:'Do you know that cute is a kind of power <3',
-    btnReply: 'Answer'
+    btnReply: 'Answer',
     reply: 'Because you have a superpower <33333333',
     messDesc: 'chị chóng khỏi ốm nhé ^.^.',
     btnAccept:  '<3',
